9f8138f0025b1093e916221205f83aad5674c212
Mai Takahashi <mai.takahashi63@gmail.com>
2016-04-11 11:44:35 -0700

    First Commit
    First Commit

---
+++ ITDCanada_CProject/april11.h
@@ -0,0 +1,14 @@
+//
+//  april11.h
+//  ITDCanada_CProject
+//
+//  Created by TakahashiMai on 2016-04-11.
+//  Copyright © 2016 TakahashiMai. All rights reserved.
+//
+
+#ifndef april11_h
+#define april11_h
+
+#include <stdio.h>
+
+#endif /* april11_h */
---
+++ ITDCanada_CProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ITDCanada_CProject
+//
+//  Created by TakahashiMai on 2016-04-11.
+//  Copyright © 2016 TakahashiMai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
