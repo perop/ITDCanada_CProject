ec95a3baecac30407ad67dcd20cf19f034cea07e
Mai Takahashi <mai.takahashi63@gmail.com>
2016-04-12 10:08:40 -0700

    Added arrayExercise
    arrayExercise

---
+++ ITDCanada_CProject/arrayExersice.c
@@ -0,0 +1,14 @@
+//
+//  arrayExersice.c
+//  ITDCanada_CProject
+//
+//  Created by TakahashiMai on 2016-04-12.
+//  Copyright © 2016 TakahashiMai. All rights reserved.
+//
+
+#include "arrayExersice.h"
+
+#define MAX_STRING "heheheh"
+
+const int SIZE = 10;
+
---
+++ ITDCanada_CProject/arrayExersice.h
@@ -0,0 +1,14 @@
+//
+//  arrayExersice.h
+//  ITDCanada_CProject
+//
+//  Created by TakahashiMai on 2016-04-12.
+//  Copyright © 2016 TakahashiMai. All rights reserved.
+//
+
+#ifndef arrayExersice_h
+#define arrayExersice_h
+
+#include <stdio.h>
+
+#endif /* arrayExersice_h */
